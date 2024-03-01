841a1f7daef2dff3de9f0cdf476184ff99912821
MSandova1 <157128090+MSandova1@users.noreply.github.com>
2024-03-01 08:19:42 -0600

    Add files via upload

---
+++ semana 7/laboratorio/L7MESA1237424.py
@@ -0,0 +1,40 @@
+# print("hola mundo")
+# print("ingrese su nombre: ")
+# nombre = input()
+
+# print("mi nombre es:" + nombre)
+
+#Ejercicio No.1
+# print("ingrese su nombre: ")
+# nombre = input()
+# print("hola " + nombre+ " que tengas un buen día Att CBUM")
+
+#ejercicio No.2
+# print("ingresa tu nombre: ")
+# nombre = input()
+# print("ingresa tu edad")
+# edad = int(input())
+# print("edad: " + str(edad))
+# print("Hola,¡" + nombre+ "! tienes " + str(edad)+ " años.")
+
+#Ejercicio No3
+# print("ingrese la temperatura en grados C: ")
+# c = int(input())
+# f = c* 9/5 + 32
+# print("la temperatura F es " + str(f))
+
+#ejercicio No 4
+# print("Ingrese el primer numero: ")
+# numero1 = int(input())
+# print("Ingrese el segundo numero: ")
+# numero2 = int(input())
+# suma = numero1 + numero2 
+# print("Suma = " + str(suma))
+
+#Ejercicio No 5
+print("Ingrese la base del triangulo")
+b = int(input())
+print ("ingrese la altura del triangulo")
+h = int(input())
+a = b* h /2
+print("area = " + str(a))
