79371ba27204a2e5c169af012af20e16f24aa2bb
MSandova1 <157128090+MSandova1@users.noreply.github.com>
2024-04-05 08:09:53 -0600

    Add files via upload

---
+++ Semana 12/laboratorio/L9MESA1237424.py
@@ -0,0 +1,53 @@
+#Programa 1
+# def main():
+#     edad=int(input("Ingrese su edad: "))
+
+#     if edad > 18:
+#         print("Debe de tramitar su DPI.")
+#     else :
+#         print("No es necesario tramitar su Dpi en estos momentos.")
+    
+# if __name__ == "__main__":
+#     main()
+
+
+#Programa 2
+# def main():
+#     numero = float(input("Ingrese un numero: "))
+
+#     if numero > 0:
+#         print("El numero ingresado es positivo")
+#     elif numero < 0:
+#         print("El numero ingresado es negativo")
+#     else:
+#         print("El numero ingresado es cero")
+# if __name__ == "__main__":
+#     main()
+
+#Programa 3
+num = int(input("ingrese un numero"))
+    num = 
+    match num:
+        case 1:
+            print("numero 1")
+        case 2: 
+            print("numero 2")
+        case 3:
+            print("numero 3")
+        case 4:
+            print("numero 4")
+        case 5:
+            print("numero 5")
+        case _:
+            print("Numero no definido")
+
+
+#programa 4
+# def main():
+#     numero = 2
+#     print("tabla de multiplicar del numero 2:")
+#     for i in range (1, 11):
+#         resultado = numero * i
+#         print(f"{numero}*{i} = {resultado}")
+# if __name__ == "__main__":
+#     main()
